37cb7a391c935b409198a4d7c695308490f94862
Lars Mogren <lars.mogren@gmail.com>
2016-07-15 07:25:27 +0200

    Fixes build for nginx-ingress-controller 0.8.1.

--- nginx-ingress-controller/build.sh
+++ nginx-ingress-controller/build.sh
@@ -4,7 +4,8 @@ set -e
 
 BASEDIR=$PWD
 K8S_BINARIES=${K8S_BINARIES:-$BASEDIR/output-armhf}
-
+INGRESS_CONTROLLER_IMAGE="kodbasen/nginx-ingress-controller-armhf:0.8.1"
+DEFAULTBACKEND_IMAGE="kodbasen/defaultbackend-armhf:1.0"
 
 git clone https://github.com/kubernetes/contrib.git
 
@@ -16,7 +17,8 @@ fi
 cd contrib/ingress/controllers/nginx
 cp $K8S_BINARIES/nginx-ingress-controller .
 sed -i -e "s;^FROM gcr.io/google_containers/nginx-slim;FROM kodbasen/nginx-slim-armhf;" "Dockerfile"
-docker build -t kodbasen/nginx-ingress-controller-armhf:0.61 .
+docker build -t $INGRESS_CONTROLLER_IMAGE .
+docker push $INGRESS_CONTROLLER_IMAGE
 
 # Build 404-server
 if [ ! -f $K8S_BINARIES/404-server ]; then
@@ -25,12 +27,14 @@ if [ ! -f $K8S_BINARIES/404-server ]; then
 fi
 cd $BASEDIR/contrib/404-server
 cp $K8S_BINARIES/404-server ./server
-sed -i -e "s;^FROM busybox;FROM hypriot/armhf-busybox;" "Dockerfile"
-docker build -t kodbasen/defaultbackend-armhf:0.0 .
+sed -i -e "s;^FROM BASEIMAGE;FROM armel/busybox;" "Dockerfile"
+docker build -t $DEFAULTBACKEND_IMAGE .
+docker push $DEFAULTBACKEND_IMAGE
 
 cd $BASEDIR
-rm -Rf contrib
+rm -rf contrib
 
+rm -f rc.yaml
 wget -q https://raw.githubusercontent.com/kubernetes/contrib/master/ingress/controllers/nginx/rc.yaml \
 && sed -i -e "s;gcr\.io/google_containers/nginx-ingress-controller;kodbasen/nginx-ingress-controller-armhf;" "rc.yaml" \
 && sed -i -e "s;gcr\.io/google_containers/defaultbackend;kodbasen/defaultbackend-armhf;" "rc.yaml"
--- nginx-slim-armhf/build.sh
+++ nginx-slim-armhf/build.sh
@@ -12,6 +12,7 @@ chmod a+x build.sh
 
 sed -i -e "s;^FROM gcr.io/google_containers/ubuntu-slim:0.3;FROM ${BASE_IMG};" "Dockerfile"
 sed -i -e "s;gcr.io/google_containers/nginx-slim;kodbasen/nginx-slim-armhf;" "Makefile"
+sed -i -e "/--with-cc-opt/d" "build.sh"
 
 make container
 
