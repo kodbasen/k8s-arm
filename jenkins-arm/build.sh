25522e2126c4f5e60a8997a8100ebb69c33d5c5f
Lars Mogren <lars.mogren@gmail.com>
2016-07-25 10:11:29 +0200

    Prepared for automatic build.

--- jenkins-arm/build.sh
+++ jenkins-arm/build.sh
@@ -2,31 +2,13 @@
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-TINI_DIR=$BASEDIR/tini
-
-if [ ! -f "$BASEDIR/tini-static" ]; then
-
-  if [ ! -d "$TINI_DIR" ]; then
-    echo "Cloning Tini!"
-    git clone https://github.com/krallin/tini.git $TINI_DIR
-  fi
-
-  cd $TINI_DIR
-  cmake . && make
-  cp tini-static $BASEDIR/
-
-fi
-
-cd $BASEDIR
-
-chmod +x tini-static
-
-if [ ! -f "$BASEDIR/init.groovy" ]; then
+if [ ! -f "$BASEDIR/tini" ]; then
+  wget --no-check-certificate -O tini https://helm.kodbasen.org/platforms/linux/arm/tini/tini-2496926
   wget https://raw.githubusercontent.com/jenkinsci/docker/195787dca586b41971e2037b48d3f6dca04c9667/init.groovy
   wget https://raw.githubusercontent.com/jenkinsci/docker/195787dca586b41971e2037b48d3f6dca04c9667/jenkins.sh
   wget https://raw.githubusercontent.com/jenkinsci/docker/195787dca586b41971e2037b48d3f6dca04c9667/install-plugins.sh
   wget https://raw.githubusercontent.com/jenkinsci/docker/195787dca586b41971e2037b48d3f6dca04c9667/plugins.sh
-  chmod +rx *.sh *.groovy
+  chmod +rx *.sh *.groovy tini
 fi
 
-docker build --rm=true --no-cache=true -t kodbasen/jenkins-arm:2.7.1 .
+#docker build --rm=true --no-cache=true -t kodbasen/jenkins-arm:2.7.1 .
