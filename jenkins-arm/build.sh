df9fb312e97ee4bdb3503769bee86de06e4b806f
Lars Mogren <lars.mogren@gmail.com>
2016-07-25 10:27:54 +0200

    Build-script downloads files to resources.

--- jenkins-arm/build.sh
+++ jenkins-arm/build.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PREFIX="kodbasen"
+VERSION=2.7.1
 
-if [ ! -f "$BASEDIR/tini" ]; then
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RESOURCEDIR="$BASEDIR/resources"
+
+if [ ! -d "$RESOURCEDIR" ]; then
+  mkdir $RESOURCEDIR && cd $RESOURCEDIR
   wget --no-check-certificate -O tini https://helm.kodbasen.org/platforms/linux/arm/tini/tini-2496926
   wget https://raw.githubusercontent.com/jenkinsci/docker/195787dca586b41971e2037b48d3f6dca04c9667/init.groovy
   wget https://raw.githubusercontent.com/jenkinsci/docker/195787dca586b41971e2037b48d3f6dca04c9667/jenkins.sh
   wget https://raw.githubusercontent.com/jenkinsci/docker/195787dca586b41971e2037b48d3f6dca04c9667/install-plugins.sh
   wget https://raw.githubusercontent.com/jenkinsci/docker/195787dca586b41971e2037b48d3f6dca04c9667/plugins.sh
-  chmod +rx *.sh *.groovy tini
+  chmod +rx *
+  cd $BASEDIR
 fi
 
-#docker build --rm=true --no-cache=true -t kodbasen/jenkins-arm:2.7.1 .
+docker build -t $PREFIX/jenkins-arm:$VERSION .
+docker tag $PREFIX/jenkins-arm:$VERSION $PREFIX/jenkins-arm:latest
+
+docker push $PREFIX/jenkins-arm:$VERSION
+docker push $PREFIX/jenkins-arm:latest
