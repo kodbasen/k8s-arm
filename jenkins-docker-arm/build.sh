ed93562862fc69c0e2e216910ec1558f7a102e80
Lars Mogren <lars.mogren@gmail.com>
2016-11-20 00:20:08 +0000

    Adds a missing taging to latest.

--- jenkins-docker-arm/build.sh
+++ jenkins-docker-arm/build.sh
@@ -5,6 +5,7 @@ IMAGE=jenkins-docker-arm
 TAG="2.19.2"
 
 docker build --rm=true --no-cache -t $PREFIX/$IMAGE:$TAG -f Dockerfile .
+docker tag $PREFIX/$IMAGE:$TAG $PREFIX/$IMAGE:latest
 docker push $PREFIX/$IMAGE:$TAG
 docker push $PREFIX/$IMAGE:latest
 
